7b4fa8d41470d6c4c51777ce2f91a42ce49cf817
Lukas Kusik <lukas.kusik@gmail.com>
2019-06-28 20:07:03 +0200

    Templatize sst method

--- build.sh
+++ build.sh
@@ -19,6 +19,12 @@ for mariadb_ver in "10.1" "10.2" "10.3"; do
         # Architecture-specific binaries
         sed -i "s|__DOCKER_ARCH__|${docker_arch}|g" Dockerfile-${mariadb_ver}.${docker_arch}
         
+        # SST method based on mariadb version
+        if [ ${mariadb_ver} == "10.1" ]; then
+            sed -i "s|__SST_METHOD__|xtrabackup-v2|g" Dockerfile-${mariadb_ver}.${docker_arch}
+        else
+            sed -i "s|__SST_METHOD__|mariabackup|g" Dockerfile-${mariadb_ver}.${docker_arch}
+        fi
         
     done
     
